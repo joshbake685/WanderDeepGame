dfac68f2f488774e6117c98404453ebf24a567d7
Joshua Baker <joshbaker685@yahoo.ca>
2025-04-11 15:51:17 -0230

    Implemented spawning locations for player, key, exit, and monster.

--- wander-deep-source/js/main.js
+++ wander-deep-source/js/main.js
@@ -2,19 +2,15 @@ import * as THREE from 'three';
 import { GameMap } from './World/GameMap.js';
 import { FirstPersonController } from './CameraController/FirstPersonController.js';
 import { OrbitCameraController } from './CameraController/OrbitCameraController.js';
+import { DebugBlock } from '../DebugBlock.js';
 
-const USE_CAMERA_ORBIT = false; // For debugging
+const USE_CAMERA_ORBIT = true; // For debugging
 
 // Create Scene
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 let cameraController = null;
 let colliders = [];
-if (USE_CAMERA_ORBIT) {
-  cameraController = new OrbitCameraController(document, renderer);
-} else {
-  cameraController = new FirstPersonController(document, renderer);
-}
 
 
 // Create clock
@@ -31,10 +27,6 @@ function init() {
 
   scene.background = new THREE.Color(0xffffff);
 
-  // Add it to your scene logic
-  scene.add(cameraController.camera);
-  scene.add(cameraController.controls.object);
-
   // Renderer
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -50,11 +42,53 @@ function init() {
 
   // Create our gameMap
   gameMap = new GameMap();
-  colliders = gameMap.dungeonWallColliders;
-  if (!USE_CAMERA_ORBIT) {
-    cameraController.colliders = colliders;
-  }
   scene.add(gameMap.gameObject);
+
+  // Update colliders with wall colliders
+  colliders = gameMap.dungeonGenerator.colliders;
+
+  if (!USE_CAMERA_ORBIT) {
+    cameraController = new FirstPersonController(document, renderer);
+    cameraController.colliders = colliders;
+    cameraController.camera.position.x = gameMap.dungeonGenerator.playerSpawn.x;
+    cameraController.camera.position.z = gameMap.dungeonGenerator.playerSpawn.z;
+  } else {
+    cameraController = new OrbitCameraController(document, renderer);
+    const playerSpawnDebugBlock = new DebugBlock(gameMap.dungeonGenerator.playerSpawn,
+      0xffff00,
+      1,
+      100,
+      1);
+    scene.add(playerSpawnDebugBlock.mesh);
+    const keySpawnDebugBlock = new DebugBlock(gameMap.dungeonGenerator.keySpawn,
+      0xff00ff,
+      1,
+      100,
+      1);
+    scene.add(keySpawnDebugBlock.mesh);
+    const exitSpawnDebugBlock = new DebugBlock(gameMap.dungeonGenerator.exitSpawn,
+      0x00ffff,
+      1,
+      100,
+      1);
+    scene.add(exitSpawnDebugBlock.mesh);
+    const monsterSpawnDebugBlock = new DebugBlock(gameMap.dungeonGenerator.monsterSpawn,
+      0x00ff00,
+      1,
+      100,
+      1);
+    scene.add(monsterSpawnDebugBlock.mesh);
+  
+
+  // Add debug blocks from dungeonGenerator
+  for (let block of gameMap.dungeonGenerator.debugBlocks) {
+    scene.add(block.mesh);
+  }
+}
+
+  // Add cameraController logic to scene
+  scene.add(cameraController.camera);
+  scene.add(cameraController.controls.object);
 
 
 
