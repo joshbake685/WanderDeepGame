61011cdd20be77dac60499af094b005efa33d00f
Joshua Baker <joshbaker685@yahoo.ca>
2025-04-16 16:56:46 -0230

    Fix path for Github pages (again)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/WanderDeep/', // ensures relative paths work for GitHub Pages
+  base: '/WanderDeepGame/', // ensures relative paths work for GitHub Pages
   build: {
     outDir: 'docs', // output will go into "docs" folder
     emptyOutDir: true
