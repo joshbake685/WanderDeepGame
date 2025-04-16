2931d5aaad7bf9ad2a7e823d5f303b22c45e5a9c
Joshua Baker <joshbaker685@yahoo.ca>
2025-04-16 16:42:45 -0230

    Build for Github pages

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './', // ensures relative paths work for GitHub Pages
+  build: {
+    outDir: 'docs', // output will go into "docs" folder
+    emptyOutDir: true
+  }
+})
